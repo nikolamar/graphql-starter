1b8e70449e2a7a9a424e3849e0f190b3c75bddaa
nikolamar <nikolamar@gmail.com>
2021-03-01 00:46:57 +0100

    refactor of file picker component

---
+++ web/src/components/file-picker.tsx
@@ -0,0 +1,23 @@
+import { Box, BoxProps } from "@chakra-ui/react";
+import { ChangeEventHandler, FC } from "react";
+import styles from "./file-picker.module.css";
+
+type FilePickerProps = Omit<BoxProps, "onChange"> & {
+  onChange?: ChangeEventHandler<{}>;
+  children: React.ReactNode;
+}
+
+export const FilePicker: FC<FilePickerProps> = ({
+  onChange,
+  children,
+  ...rest
+}) => (
+  <Box {...rest} className={styles.container}>
+    {children}
+    <input
+      type="file"
+      onChange={onChange}
+      className={styles.input}
+    />
+  </Box>
+);
